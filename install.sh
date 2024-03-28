c169b11c365d024e70b18fa927e8d6ae03b7db5c
Steve Miller <me@r15cookie.com>
2024-03-28 09:01:08 -0400

    Initial starship installation

--- install.sh
+++ install.sh
@@ -20,6 +20,11 @@ if ! chezmoi="$(command -v chezmoi)"; then
   unset chezmoi_install_script bin_dir
 fi
 
+# Install starship
+if ! starship="$(command -v starship)"; then
+  curl -sS https://starship.rs/install.sh | sh
+fi
+
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
